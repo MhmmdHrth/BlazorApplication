8140c5c098d559175b432bcea387201c56d31691
Muhammad Harith <harith781@gmail.com>
2021-07-04 22:36:15 +0800

    department API

--- EmployeeManagement.API/Controllers/DepartmentController.cs
+++ EmployeeManagement.API/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EmployeeManagement.API.Models;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,38 @@ namespace EmployeeManagement.API.Controllers
     [ApiController]
     public class DepartmentController : ControllerBase
     {
+        private readonly IDepartment departmentRepo;
+
+        public DepartmentController(IDepartment departmentRepo)
+        {
+            this.departmentRepo = departmentRepo;
+        }
+
+        [HttpGet(nameof(GetDepartments))]
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartments()
+        {
+            try
+            {
+                return Ok(await departmentRepo.GetDepartments());
+            }
+            catch (Exception err)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"{err.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> GetEmployee(int id)
+        {
+            try
+            {
+                var obj = await departmentRepo.GetDepartment(id);
+                return obj == null ? NotFound() : Ok(obj);
+            }
+            catch (Exception err)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"{err.Message}");
+            }
+        }
     }
 }
